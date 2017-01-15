82760fd172118d588823045fc00b0b5048d8c766
Chris Polychronides <cpoly13@gmail.com>
2017-01-15 01:45:45 -0500

    Completed

---
+++ UNC Comp 410/src/Assignment0/RunTests.java
@@ -0,0 +1,10 @@
+package Assignment0;
+import gradingTools.comp410s17.assignment0.testcases.Assignment0Suite;
+
+public class RunTests {
+		  public static void main(String[] args){
+		    Assignment0Suite.main(args);
+		  }
+
+
+}
