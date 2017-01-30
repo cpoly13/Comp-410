f54cf7e86efa9106a637d3180ae11a073bc960c6
Chris <Chris@Chris-PC>
2017-01-29 22:58:55 -0500

    Beta

--- Assignment1/src/LinkedList_A1/LinkedListImpl.java
+++ Assignment1/src/LinkedList_A1/LinkedListImpl.java
@@ -6,51 +6,153 @@
 package LinkedList_A1;
 
 public class LinkedListImpl implements LIST_Interface {
-  Node root;//this will be the entry point to your linked list (the head)
-  
-  public LinkedListImpl(){//this constructor is needed for testing purposes. Please don't modify!
-    root=new Node(0); //Note that the root's data is not a true part of your data set!
-  }
-  
-  //implement all methods in interface, and include the getRoot method we made for testing purposes. Feel free to implement private helper methods!
-  
-  public Node getRoot(){ //leave this method as is, used by the grader to grab your linkedList easily.
-    return root;
-  }
+	Node root;// this will be the entry point to your linked list (the head)
+	private int listSize = 0;
 
-@Override
-public boolean insert(Node n, int index) {
-	// TODO Auto-generated method stub
-	return false;
-}
+	public LinkedListImpl() {// this constructor is needed for testing purposes.
+								// Please don't modify!
+		root = new Node(0); // Note that the root's data is not a true part of
+							// your data set!
+	}
 
-@Override
-public boolean remove(int index) {
-	// TODO Auto-generated method stub
-	return false;
-}
+	// implement all methods in interface, and include the getRoot method we
+	// made for testing purposes. Feel free to implement private helper methods!
 
-@Override
-public Node get(int index) {
-	// TODO Auto-generated method stub
-	return null;
-}
+	public Node getRoot() { // leave this method as is, used by the grader to
+							// grab your linkedList easily.
+		return root;
+	}
 
-@Override
-public int size() {
-	// TODO Auto-generated method stub
-	return 0;
-}
+	@Override
+	public boolean insert(Node n, int index) {
 
-@Override
-public boolean isEmpty() {
-	// TODO Auto-generated method stub
-	return false;
-}
+		if (index > listSize + 1 || index < 0) { // returns false for faulty
+													// index values
+			return false;
+		}
 
-@Override
-public void clear() {
-	// TODO Auto-generated method stub
-	
-}
+		else {
+			Node temp = root;
+			if (index < listSize) { // if body for insertions
+													// within the list but not
+													// on ends
+				for (int count = 0; count != index; count++) {
+					temp = temp.next;
+				}
+
+				n.prev = temp;
+				n.next = temp.next;
+				temp.next = n;
+				if(index!=0){
+				n.next.prev = n;
+				}
+				listSize++;
+
+				return true;
+			}
+
+			else {
+				for (int count = 0; count != index; count++) { // if body for insertions at the beginning
+																// or end of
+																// list
+					temp = temp.next;
+				}
+
+				n.prev = temp;
+				n.next = null;
+				temp.next = n;
+
+				listSize++;
+
+				return true;
+
+			}
+
+		}
+
+	}
+
+	@Override
+	public boolean remove(int index) {
+		// TODO Auto-generated method stub
+		if (index >= listSize || index < 0) {
+			return false;
+		}
+
+		else {
+			
+			Node temp=root.next;
+			
+			for( int count=0; count!=index; count++){
+				temp=temp.next;
+			}
+			
+			try{
+				temp.next.prev=temp.prev;}
+				
+			catch(NullPointerException e){}
+				 temp.prev.next=temp.next;
+				    listSize--; 
+				    return true;
+			
+			
+			   
+			
+			/*if (temp.next.next.equals(null)){
+				temp.next.prev=null;
+				temp.next=null;
+			}
+			
+			else{
+				temp.next.next=null;
+				temp.next.prev=null;
+				temp.next=null;
+			  }*/
+
+		}
+	}
+
+	@Override
+	public Node get(int index) {
+		// TODO Auto-generated method stub
+		
+		if(index<0 || index>= listSize || listSize==0){
+		return null;
+		}
+		
+		else{
+			
+			Node temp=root.next;
+			
+			for (int count=0; count!=index; count ++){
+				temp=temp.next;
+			}
+			
+			return temp;
+			
+		}
+	}
+
+	@Override
+	public int size() {
+		
+		return listSize;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		
+		return listSize==0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+		if (listSize>0){
+			root.next=null;
+			listSize=0;
+		}
+
+	}
 }
