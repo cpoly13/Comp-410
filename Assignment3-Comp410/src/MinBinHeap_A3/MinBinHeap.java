31a9274c94dadc78929de69c268af3e6e928a351
Chris Polychronides <cpoly13@gmail.com>
2017-03-01 18:38:18 -0500

    Bare skeleton

---
+++ Assignment3-Comp410/src/MinBinHeap_A3/MinBinHeap.java
@@ -0,0 +1,22 @@
+package MinBinHeap_A3;
+
+public class MinBinHeap implements Heap_Interface {
+  private EntryPair[] array; //load this array
+  private int size;
+  private static final int arraySize = 10000; //Everything in the array will initially 
+                                              //be null. This is ok! Just build out 
+                                              //from array[1]
+
+  public MinBinHeap() {
+    this.array = new EntryPair[arraySize];
+    array[0] = new EntryPair(null, -100000); //0th will be unused for simplicity 
+                                             //of child/parent computations...
+                                             //the book/animation page both do this.
+  }
+    
+  //Please do not remove or modify this method! Used to test your entire Heap.
+  @Override
+  public EntryPair[] getHeap() { 
+    return this.array;
+  }
+}
