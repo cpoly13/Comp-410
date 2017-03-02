b4dc8db08d487dd14b6f36bea1c15945703dfd64
Chris Polychronides <cpoly13@gmail.com>
2017-03-01 19:48:40 -0500

    Started implementation of insert

--- Assignment3-Comp410/src/MinBinHeap_A3/MinBinHeap.java
+++ Assignment3-Comp410/src/MinBinHeap_A3/MinBinHeap.java
@@ -3,6 +3,7 @@ package MinBinHeap_A3;
 public class MinBinHeap implements Heap_Interface {
   private EntryPair[] array; //load this array
   private int size;
+  private int hole; //CP tracks current hole location
   private static final int arraySize = 10000; //Everything in the array will initially 
                                               //be null. This is ok! Just build out 
                                               //from array[1]
@@ -11,7 +12,7 @@ public class MinBinHeap implements Heap_Interface {
     this.array = new EntryPair[arraySize];
     array[0] = new EntryPair(null, -100000); //0th will be unused for simplicity 
                                              //of child/parent computations...
-                                             //the book/animation page both do this.
+                                            //the book/animation page both do this.
   }
     
   //Please do not remove or modify this method! Used to test your entire Heap.
@@ -23,6 +24,25 @@ public class MinBinHeap implements Heap_Interface {
 @Override
 public void insert(EntryPair entry) {
 	// TODO Auto-generated method stub
+	hole=size+1;
+	if(size==0){
+		array[1]=entry;
+	}
+	
+	else{
+		if(array[hole-1].getPriority()<=entry.getPriority()){
+			array[hole]=entry;
+		}
+		
+		else{
+			while(array[hole].getPriority()>entry.getPriority()){
+				hole=hole/2;
+			}
+			
+			array[hole]=entry;
+			
+		}
+	}
 	
 	
 }
